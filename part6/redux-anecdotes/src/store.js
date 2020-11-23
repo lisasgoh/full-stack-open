ba620523e226334e754968c6a2d97e7088238780
lisasgoh <42923450+lisasgoh@users.noreply.github.com>
2020-11-24 03:42:11 +0800

    Initialise anecdotes in backend

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -5,7 +5,6 @@ import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
 
-
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
   notification: notificationReducer,
